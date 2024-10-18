9d930f4e20e8a1cb4afc487f83b3a38272262d1f
Manalan Kumarasamy Muruganandam <kmmanalan01@gmail.com>
2024-10-19 10:30:59 +1100

    feature:task-creator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TaskPanelComponent } from "./task-panel/task-panel.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [TaskPanelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
