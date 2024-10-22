5a106d49cf804853c8349770a8ce568a905a210f
Manalan Kumarasamy Muruganandam <kmmanalan01@gmail.com>
2024-10-22 19:02:16 +1100

    WIP: almost done

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -7,7 +7,25 @@ import { Task } from '../../model/Task';
 export class TaskService {
   private tasks : Task[]
   constructor() { 
-    this.tasks = []
+    this.tasks = [
+      {
+        TaskContent: "task 1",
+        TaskPriority: "imp-not-urgent"
+      },
+      {
+        TaskContent: "task 2",
+        TaskPriority: "imp-urgent"
+      }
+      ,
+      {
+        TaskContent: "task 3",
+        TaskPriority: "not-imp-not-urgent"
+      },
+      { 
+        TaskContent: "task4",
+        TaskPriority: "not-imp-urgent"
+      }
+    ]
   }
 
   addTasks(newTask:Task) { 
--- src/app/task-panel/task-board/task-board.component.ts
+++ src/app/task-panel/task-board/task-board.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../service/task.service';
+import { Task } from '../../../model/Task';
 
 @Component({
   selector: 'app-task-board',
@@ -7,6 +9,13 @@ import { Component } from '@angular/core';
   templateUrl: './task-board.component.html',
   styleUrl: './task-board.component.css'
 })
-export class TaskBoardComponent {
+export class TaskBoardComponent implements OnInit {
+  createdTasks: Task[] = []
 
+  constructor (private taskService : TaskService) { 
+  }
+
+  ngOnInit(): void {
+    this.createdTasks = this.taskService.getTask()
+  }
 }
