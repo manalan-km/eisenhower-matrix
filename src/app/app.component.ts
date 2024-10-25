ea5055dee73ae0a160934bcaa3cb203b5b5d42bf
Manalan Kumarasamy Muruganandam <kmmanalan01@gmail.com>
2024-10-26 10:17:48 +1100

    feature/edit-display-tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { TaskPanelComponent } from "./task-panel/task-panel.component";
+import { RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TaskPanelComponent],
+  imports: [TaskPanelComponent, RouterModule,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -5,10 +5,16 @@ import { Task } from '../../model/Task';
   providedIn: 'root'
 })
 export class TaskService {
-  private TaskId = 0
+  private TaskId
   private tasks : Task[]
   constructor() { 
-    this.tasks = []
+    this.tasks = [
+      { id: 1, TaskContent: "task 1", TaskPriority: "imp-not-urgent" },
+      { id: 2, TaskContent: "task 2", TaskPriority: "imp-urgent" },
+      { id: 3, TaskContent: "task 3", TaskPriority: "not-imp-not-urgent" },
+      { id: 4, TaskContent: "task 4", TaskPriority: "not-imp-urgent" }
+  ];
+  this.TaskId = this.tasks.length
   }
 
   createTaskId() { 
@@ -17,15 +23,34 @@ export class TaskService {
 
   addTasks(task:Partial<Task>) {
     const newTask:Task = { 
+      id: this.createTaskId(),
       TaskContent : task.TaskContent!,
       TaskPriority: task.TaskPriority!,
-      id: this.createTaskId()
     }
     this.tasks.push(newTask)
     console.log(this.tasks)
   }
   
-  getTask() {
+  getTasks() {
     return this.tasks
+  }
+
+  getTask(TaskId:number) { 
+    console.log(this.tasks[TaskId -1])
+    return this.tasks[TaskId - 1]
+    
+  }
+
+  updateTasks(task:Partial<Task>, taskId:number) {
+    const index = taskId - 1
+    this.tasks[index].TaskContent = task.TaskContent!
+    this.tasks[index].TaskPriority = task.TaskPriority!
+  }
+
+  removeTask(taskId:number) { 
+    this.tasks = this.tasks.filter((task)=>{
+      return task.id !== taskId
+    })
+    console.log(this.tasks)
   }
 }
