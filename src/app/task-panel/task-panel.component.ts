7840001d29f181e107a548de693d187435afad4f
Manalan Kumarasamy Muruganandam <kmmanalan01@gmail.com>
2024-10-26 09:57:11 +1100

    restore edit component

--- src/app/task-panel/task-panel.component.ts
+++ src/app/task-panel/task-panel.component.ts
@@ -9,7 +9,6 @@ import { TaskBoardComponent } from "./task-board/task-board.component";
   templateUrl: './task-panel.component.html',
   styleUrl: './task-panel.component.css'
 })
-
 export class TaskPanelComponent {
 
 }
