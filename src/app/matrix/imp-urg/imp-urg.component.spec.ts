a42211d53f7271beb35bd9b44be6777219c20fa5
Manalan Kumarasamy Muruganandam <kmmanalan01@gmail.com>
2024-10-26 10:50:57 +1100

    matrix and imp-urg setup

---
+++ src/app/matrix/imp-urg/imp-urg.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ImpUrgComponent } from './imp-urg.component';
+
+describe('ImpUrgComponent', () => {
+  let component: ImpUrgComponent;
+  let fixture: ComponentFixture<ImpUrgComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ImpUrgComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ImpUrgComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/matrix/imp-urg/imp-urg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imp-urg',
+  standalone: true,
+  imports: [],
+  templateUrl: './imp-urg.component.html',
+  styleUrl: './imp-urg.component.css'
+})
+export class ImpUrgComponent {
+
+}
