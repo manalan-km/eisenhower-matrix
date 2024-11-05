cf0e861716e528850ea4ac86f71baa8d9a5f4970
Manalan Kumarasamy Muruganandam <kmmanalan01@gmail.com>
2024-11-05 13:46:03 +1100

    chore:added taskStatus to Task model

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -6,71 +6,71 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class TaskService {
-  private TaskId
-  private tasks : Task[]
-  private taskSubject  :BehaviorSubject<Task[]>
-  
-  constructor() { 
+  private TaskId;
+  private tasks: Task[];
+  private taskSubject: BehaviorSubject<Task[]>;
+
+  constructor() {
     this.tasks = [
-      { id: 1, TaskContent: "task 1", TaskPriority: "imp-not-urgent" },
-      { id: 2, TaskContent: "task 2", TaskPriority: "imp-urgent" },
-      { id: 3, TaskContent: "task 3", TaskPriority: "not-imp-not-urgent" },
-      { id: 4, TaskContent: "task 4", TaskPriority: "not-imp-urgent" },
-      { id: 5, TaskContent: "task 5", TaskPriority: "imp-not-urgent" },
-      { id: 6, TaskContent: "task 6", TaskPriority: "imp-urgent" },
-      { id: 7, TaskContent: "task 7", TaskPriority: "not-imp-not-urgent" },
-      { id: 8, TaskContent: "task 8", TaskPriority: "not-imp-urgent" }
+      { id: 1, TaskContent: 'task 1', TaskPriority: 'imp-not-urgent', TaskStatus: 'in-progress' },
+      { id: 2, TaskContent: 'task 2', TaskPriority: 'imp-urgent', TaskStatus: 'in-progress' },
+      { id: 3, TaskContent: 'task 3', TaskPriority: 'not-imp-not-urgent', TaskStatus: 'in-progress' },
+      { id: 4, TaskContent: 'task 4', TaskPriority: 'not-imp-urgent', TaskStatus: 'in-progress' },
+      { id: 5, TaskContent: 'task 5', TaskPriority: 'imp-not-urgent', TaskStatus: 'in-progress' },
+      { id: 6, TaskContent: 'task 6', TaskPriority: 'imp-urgent', TaskStatus: 'in-progress' },
+      { id: 7, TaskContent: 'task 7', TaskPriority: 'not-imp-not-urgent', TaskStatus: 'in-progress' },
+      { id: 8, TaskContent: 'task 8', TaskPriority: 'not-imp-urgent', TaskStatus: 'in-progress' },
     ];
 
-    this.taskSubject  = new BehaviorSubject<Task[]>(this.tasks)
-    this.TaskId = this.tasks.length
+    this.taskSubject = new BehaviorSubject<Task[]>(this.tasks);
+    this.TaskId = this.tasks.length;
   }
 
-  private createTaskId() { 
-    return this.TaskId = this.TaskId + 1
+  private createTaskId() {
+    return (this.TaskId = this.TaskId + 1);
   }
 
-  private sendUpdate() { 
-    this.taskSubject.next(this.tasks)
+  private sendUpdate() {
+    this.taskSubject.next(this.tasks);
   }
 
-  addTasks(task:Partial<Task>) {
-    const newTask:Task = { 
+  addTasks(task: Partial<Task>) {
+    const newTask: Task = {
       id: this.createTaskId(),
       TaskContent: task.TaskContent!,
       TaskPriority: task.TaskPriority!,
-    }
+      TaskStatus: 'in-progress',
+    };
 
-    this.tasks.push(newTask)
-    this.sendUpdate()
-    console.log(this.tasks)
+    this.tasks.push(newTask);
+    this.sendUpdate();
+    console.log(this.tasks);
   }
-  
+
   getTaskObservable() {
-    const taskObservable = this.taskSubject.asObservable()
-    return taskObservable
+    const taskObservable = this.taskSubject.asObservable();
+    return taskObservable;
   }
 
-  getTaskById(TaskId:number) { 
-    console.log(this.tasks[TaskId -1])
-    return this.tasks[TaskId - 1]
+  getTaskById(TaskId: number) {
+    console.log(this.tasks[TaskId - 1]);
+    return this.tasks[TaskId - 1];
   }
 
-  updateTasks(task:Partial<Task>, taskId:number) {
-    const index = taskId - 1
-    this.tasks[index].TaskContent = task.TaskContent!
-    this.tasks[index].TaskPriority = task.TaskPriority!
+  updateTasks(task: Partial<Task>, taskId: number) {
+    const index = taskId - 1;
+    this.tasks[index].TaskContent = task.TaskContent!;
+    this.tasks[index].TaskPriority = task.TaskPriority!;
 
-    this.sendUpdate()
-    
+    this.sendUpdate();
   }
 
-  removeTask(taskId:number) { 
-    this.tasks = this.tasks.filter((task)=>{
-      return task.id !== taskId
-    })
+  removeTask(taskId: number) {
+    this.tasks = this.tasks.filter((task) => {
+      return task.id !== taskId;
+    });
 
-    this.sendUpdate()
-    console.log(this.tasks)
+    this.sendUpdate();
+    console.log(this.tasks);
   }
 }
--- src/model/Task.ts
+++ src/model/Task.ts
@@ -1,7 +1,8 @@
 export type Priority = 'no-priority' | 'imp-urgent' | 'imp-not-urgent' | 'not-imp-urgent' | 'not-imp-not-urgent';
-
+export type taskStatus = 'completed' | 'in-progress';
 export type Task = {
   id: number;
   TaskContent: string;
   TaskPriority: Priority;
+  TaskStatus: taskStatus;
 };
