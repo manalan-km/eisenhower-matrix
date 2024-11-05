7f4c0c84f6436e0dd7431184c322d09bfdf94417
Manalan Kumarasamy Muruganandam <kmmanalan01@gmail.com>
2024-11-05 17:56:28 +1100

    feature:nav and footer

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,5 +1,5 @@
 export const PRIORITY = [
-  { priority: 'no-priority', value: '' },
+  { priority: 'no-priority', value: 'No Priority' },
   { priority: 'imp-urgent', value: 'Important and Urgent' },
   { priority: 'imp-not-urgent', value: 'Important but not Urgent' },
   { priority: 'not-imp-urgent', value: 'Not Important but Urgent' },
